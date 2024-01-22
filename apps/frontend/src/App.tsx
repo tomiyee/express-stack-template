55cf438e1ccef1b7f9c3c9f102271ea06b33ecbd
Tommy S Heng <tommy.seng.heng@gmail.com>
2024-01-21 23:28:37 -0500

    temp

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { DEFAULT_NUMBER } from 'common';
+import {DEFAULT_NUMBER} from 'common';
 
 function App() {
   const DEFAULT_INCREMENT = 2;
