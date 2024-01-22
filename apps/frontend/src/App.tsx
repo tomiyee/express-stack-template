158d589a91bc9b6949dddc00c77cbc4348b1e9af
Tom <tommy.seng.heng@gmail.com>
2024-01-22 01:17:06 -0500

    works on linux

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -2,11 +2,11 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import {DEFAULT_NUMBER} from 'common';
+import { DEFAULT_COUNTER_VALUE } from 'common';
 
 function App() {
   const DEFAULT_INCREMENT = 2;
-  const [count, setCount] = useState(DEFAULT_NUMBER)
+  const [count, setCount] = useState(DEFAULT_COUNTER_VALUE);
 
   return (
     <>
