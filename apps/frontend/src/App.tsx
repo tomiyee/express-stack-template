3f657d0805cd2c1111422e0dfbbc2717622296b3
Tommy S Heng <tommy.seng.heng@gmail.com>
2024-01-23 05:15:26 -0500

    feat(infra): lint and style

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,7 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 import { DEFAULT_COUNTER_VALUE, DEFAULT_INCREMENT } from 'common';
 
 function App() {
@@ -30,7 +30,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
