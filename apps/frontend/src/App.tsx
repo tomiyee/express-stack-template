870a2e425175ac1d0ce833fc16af3e8307a2fd70
Tommy S Heng <tommy.seng.heng@gmail.com>
2024-01-24 06:59:27 -0500

    feat(frontend): configure to allow env vars

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 import { DEFAULT_COUNTER_VALUE, DEFAULT_INCREMENT } from 'common';
+import { API_URL } from './constants';
 
 function App() {
   const [count, setCount] = useState(DEFAULT_COUNTER_VALUE);
@@ -28,6 +29,9 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
+      </p>
+      <p>
+        API URL is <code>{API_URL}</code>
       </p>
     </>
   );
