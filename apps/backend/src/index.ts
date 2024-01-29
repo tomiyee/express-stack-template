cbb1cbbda0958f1c3607c6f987fe28875d76aeba
Tommy Heng <tommy.seng.heng@gmail.com>
2024-01-28 23:23:08 -0500

    more backend stuff

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,17 +1,17 @@
-import express from "express";
-import cors from "cors";
-import {createServer} from "http";
-import usersRouter from "./routes/users.js";
+import express from 'express';
+import cors from 'cors';
+import { createServer } from 'http';
+import usersRouter from './routes/users.js';
 
 const PORT = process.env.PORT || 8080;
 
 const app = express();
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.use("/api/users", usersRouter);
+app.use('/api/users', usersRouter);
 
 const httpServer = createServer(app);
 
-httpServer.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
-});
+httpServer.listen(PORT, () => console.log(`Listening on port ${PORT}`));
--- apps/backend/src/routes/users.ts
+++ apps/backend/src/routes/users.ts
@@ -1,14 +1,14 @@
-import express, { Request, Response } from "express";
+import express, { Request, Response } from 'express';
 
 const usersRouter = express.Router();
 
 /**
  * Returns a list of users.
- * 
+ *
  * @name GET /api/users
  */
-usersRouter.get("/", (req: Request, res: Response) => {
-  res.send("Hello from users!");
+usersRouter.get('/', (req: Request, res: Response) => {
+  res.send('Hello from users!');
 });
 
 export default usersRouter;
