98bec4d7db5b2fa0e35f2445266ef1a21b5e4f7b
Tommy Heng <tommy.seng.heng@gmail.com>
2024-01-28 23:29:35 -0500

    docstrings

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -2,6 +2,11 @@ import express from 'express';
 import cors from 'cors';
 import { createServer } from 'http';
 import usersRouter from './routes/users.js';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const PORT = process.env.PORT || 8080;
 
@@ -10,8 +15,19 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+// --- Sets up REST API endpoints --- //
+
 app.use('/api/users', usersRouter);
 
-const httpServer = createServer(app);
+// --- Sets up static files for serving the built frontend --- //
 
+app.use(express.static(path.join(__dirname, '../../frontend/dist')));
+// Serve the compiled React code for all other routes
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../frontend/dist/index.html'));
+});
+
+// --- Host the server --- //
+
+const httpServer = createServer(app);
 httpServer.listen(PORT, () => console.log(`Listening on port ${PORT}`));
