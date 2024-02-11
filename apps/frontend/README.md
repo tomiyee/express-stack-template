36bbd29d5948e5a38e65f672cbbb1ae235c4a0e9
Tommy S Heng <tommy.seng.heng@gmail.com>
2024-02-11 00:07:27 -0500

    feat(frontend): login and register view

--- apps/frontend/README.md
+++ apps/frontend/README.md
@@ -5,3 +5,7 @@
 You can add environment variables to the frontend by first updating the `vite-env.d.ts` file, which gives type intellisense.
 
 To specify what URL the backend is, you can specify it through the environment variable.
+
+## Todo When Reusing Template
+
+- [ ] Change the title in `/frontend/src/index.html`
---
+++ apps/frontend/src/views/LogInView.tsx
@@ -0,0 +1,55 @@
+import { Button, Stack, TextField, Typography } from '@mui/material';
+import { assertUnreached } from 'common';
+import React, { useState } from 'react';
+
+enum LoginMode {
+  LOGIN,
+  REGISTER,
+}
+
+const LoginView: React.FC = () => {
+  const [loginMode, setLoginMode] = useState(LoginMode.LOGIN);
+  return (
+    <Stack spacing={1}>
+      <Typography variant="h1">Welcome to the Todo List</Typography>
+      {loginMode === LoginMode.LOGIN ? (
+        <LoginForm setMode={setLoginMode} />
+      ) : loginMode === LoginMode.REGISTER ? (
+        <SignupForm setMode={setLoginMode} />
+      ) : (
+        assertUnreached(loginMode)
+      )}
+    </Stack>
+  );
+};
+export default LoginView;
+
+const LoginForm: React.FC<{ setMode: (arg0: LoginMode) => void }> = ({
+  setMode,
+}) => {
+  return (
+    <Stack>
+      <TextField label="Username" />
+      <TextField label="Password" />
+      <Button variant="contained">Log in</Button>
+      <Button onClick={() => setMode(LoginMode.REGISTER)}>
+        Don't have an account?
+      </Button>
+    </Stack>
+  );
+};
+
+const SignupForm: React.FC<{ setMode: (arg0: LoginMode) => void }> = ({
+  setMode,
+}) => {
+  return (
+    <Stack>
+      <TextField label="Username" />
+      <TextField label="Password" />
+      <Button variant="contained">Sign up</Button>
+      <Button onClick={() => setMode(LoginMode.LOGIN)}>
+        Already have an account?
+      </Button>
+    </Stack>
+  );
+};
